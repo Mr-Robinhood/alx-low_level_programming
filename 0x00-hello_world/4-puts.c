0c84d46cab4f2648b535c6a72148886aa647fcb8
Mr-Robinhood <donomer98@gmail.com>
2023-12-15 02:42:03 +0200

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main -A program is to write a paragraph using puts
  * return = (0) success
  */
-int mainbetty-style(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
