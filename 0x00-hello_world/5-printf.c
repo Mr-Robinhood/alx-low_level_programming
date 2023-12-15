569ea0e7a3fa47323e9e0ba7459b9e42e0d0c2c2
Mr-Robinhood <donomer98@gmail.com>
2023-12-15 02:54:28 +0200

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stio.h>
+/**
+ * main - A program is to write a paragraph using printf function
+ * return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+        return (0);
+}
+
