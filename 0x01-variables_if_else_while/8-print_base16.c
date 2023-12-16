9b39fcbfd01f4a938cefa09f788c18cdb467845b
Mr-Robinhood <donomer98@gmail.com>
2023-12-16 04:34:31 +0200

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,10 @@ int main(void)
 {
 char letter;
 int num;
-for (num = '0'; num < 10; num++)
+for (num = 0; num < 10; num++)
 putchar((num % 10) + '0');
 for (letter = 'a'; letter <= 'f'; letter++)
 putchar(letter);
 putchar('\n');
 return (0);
-}         
+}
