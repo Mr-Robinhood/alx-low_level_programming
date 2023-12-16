09a761cb59efc5118f9d2b9327665658d5ffb514
Mr-Robinhood <donomer98@gmail.com>
2023-12-16 03:25:44 +0200

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+        char c;
+
+        c = 'a';
+
+	m ='A';
+        while
+           (c <= 'z' && m <= 'Z') {
+                        putchar(c);
+			putchar(m);
+                        c++;
+			m++;
+                }
+        putchar('\n');
+        return (0);
+}
+~                
