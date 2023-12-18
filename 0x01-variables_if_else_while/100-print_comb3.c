b70cd0b716eb1f319c687b55c21ea650f95e2fe6
Mr-Robinhood <donomer98@gmail.com>
2023-12-18 02:39:17 +0200

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,32 +6,23 @@
  */
 int main(void)
 {
-	int digit1, digit2;
-	digit1 = 0;
-	digit2 = 0;
-
-	while(digit2 <= 9)
-	{
-		if (!(digit1 > digit2) || digit1 == digit2)
-		{
-		putchar(digit1);
-		putchar(digit1);
-		
-		if (digit1 == '8' && digit2 == '9')
-		{
-			putchar('\n');
-		
-		}
-		else{
-			putchar(',');
-			putchar(' ');
-		}
-		
-	}
-	digit2++;
-	}
-	digit2 = '0';
-	digit1 = '0';
+int digit1, digit2;
+for(digit1 = '0'; digit1 < '9'; digit1++)
+{
+for(digit2 = digit1 + 1; digit2 <= '9'; digit2++)
+{
+if (digit2 != digit1)
+{	
+putchar(digit1);
+putchar(digit2);		
+if (digit1 == '8' && digit2 == '9')
+continue;
+putchar(',');
+putchar(' ');
+}		
+}
+}
+putchar ('\n');
 
 	return (0);
 }
