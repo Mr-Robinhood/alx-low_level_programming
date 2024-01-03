14fa2f0a2dd57d46cda033e6426eb1589da09138
Mr-Robinhood <donomer98@gmail.com>
2024-01-03 02:05:04 +0200

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98.c - takes the pointer to an int parameter
+ * 		and update the value to 98.
+ * @n: int parameter
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
