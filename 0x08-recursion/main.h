77ea6ff329eb1135145dfdfee94573addc94b8b8
Mr-Robinhood <donomer98@gmail.com>
2024-01-11 03:37:54 +0200

    task 7

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,5 +14,7 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _sqrt(int n, int x);
 int is_prime(int n, int start);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif /*MAIN_H*/
