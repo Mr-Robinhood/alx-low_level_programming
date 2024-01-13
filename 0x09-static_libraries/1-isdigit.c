40444dfec0f7e925a4026f47f3ed1df83118497c
Mr-Robinhood <donomer98@gmail.com>
2024-01-13 04:39:17 +0200

    task0

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check for a digit 0 through 9 .
+ *@c : input for digit .
+ * Return: 1 if c is digit and 0 if else .
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - A function check if character is lowercase or uppercase
+ * @c: check input of function
+ * Return: return 1 if c is lowercase otherwise 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
