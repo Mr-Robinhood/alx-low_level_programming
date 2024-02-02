9260bd6796ad2339f273ff3bf486790e5d0c4990
Mr-Robinhood <donomer98@gmail.com>
2024-02-02 03:37:26 +0200

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,39 +1,21 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list
- *@head: pointer to the head of the list
- *@str: string to be added
+ * list_len - finds the number of elements in a linked list
+ *@h: pointer to the list
  *
- * Return: returns the address to the new element or NULL
- * if failed
+ *Return: the number of elements
 */
 
-list_t *add_node(list_t **head, const char *str)
-{
-	char *dup;
-	int len;
-	list_t *new;
-
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-
-	dup = strdup(str);
-	if (dup == NULL)
+size_t list_len(const list_t *h)
 	{
-		free(new);
-		return (NULL);
+		size_t elements = 0;
+
+		while (h)
+		{
+			elements++;
+			h = h->next;
+		}
+
+		return (elements);
 	}
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = dup;
-	new->len = len;
-	new->next = *head;
-
-	*head = new;
-
-	return (new);
-
-}
